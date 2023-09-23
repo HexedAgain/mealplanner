b46ee6883ff5beca0127590c70d07f1110f8ea6d
HexedAgain <gregory.astley@gmail.com>
2023-09-23 01:07:48 +0100

    got kotest running

--- data/repository/src/test/java/com/example/data/repository/ExampleUnitTest.kt
+++ data/repository/src/test/java/com/example/data/repository/ExampleUnitTest.kt
@@ -3,22 +3,22 @@ package com.example.data.repository
 //import org.junit.Test
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
+//import org.junit.jupiter.api.Test
 
-import org.junit.Assert.*
+//import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-//        assertEquals(4, 2 + 3)
-        true shouldBe false
-    }
-}
+//class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+////        assertEquals(4, 2 + 3)
+//        true shouldBe false
+//    }
+//}
 
 class Kotest: FreeSpec() {
     init {
