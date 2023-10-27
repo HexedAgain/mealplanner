64c6521ee6de950353ecbb87f69f8a65ad79f42b
HexedAgain <gregory.astley@gmail.com>
2023-10-27 22:50:11 +0100

    experiment with ImageDecoder

--- feature/lab/src/main/java/com/example/lab/ui/LabScreen.kt
+++ feature/lab/src/main/java/com/example/lab/ui/LabScreen.kt
@@ -1,5 +1,11 @@
 package com.example.lab.ui
 
+import android.graphics.Bitmap
+import android.graphics.ColorSpace
+import android.graphics.DrawFilter
+import android.graphics.ImageDecoder
+import android.graphics.ImageDecoder.OnHeaderDecodedListener
+import android.media.Image
 import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -10,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.core.graphics.decodeBitmap
 import androidx.core.net.toFile
 import com.example.core.navigation.Navigator
 import com.example.core.ui.ThemedAppBarScreen
@@ -20,6 +27,9 @@ import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import java.io.File
 import java.net.URI
@@ -54,16 +64,30 @@ fun GetRecipeFromImage() {
 
     val getContent = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
         uri?.let {
-            val recogniser: TextRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-            val imageBitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
-            val image = InputImage.fromBitmap(imageBitmap, 0)
-            recogniser.process(image)
-                .addOnSuccessListener {
-                    val a = 5
+            CoroutineScope(Dispatchers.IO).launch {
+                val recogniser: TextRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+//                val imageBitmap: Bitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+                val imageSource: ImageDecoder.Source = ImageDecoder.createSource(context.contentResolver, uri)
+                val imageBitmap: Bitmap = imageSource.decodeBitmap {x, y ->
+                    this.setTargetSize(x.size.width / 2, x.size.height / 2)
+                    this.setPostProcessor { it ->
+                        it.drawFilter = DrawFilter()
+                        1
+                    }
                 }
-                .addOnFailureListener {
-                    val a = 5
-                }
+//                val imageBitmap: Bitmap = ImageDecoder.decodeBitmap(imageSource)
+//                val imageBitmap: Bitmap = ImageDecoder.decodeBitmap(imageSource) { decoder, info, source ->
+//                    TODO("Not yet implemented")
+//                }
+                val image = InputImage.fromBitmap(imageBitmap, 0)
+                recogniser.process(image)
+                    .addOnSuccessListener {
+                        val a = 5
+                    }
+                    .addOnFailureListener {
+                        val a = 5
+                    }
+            }
         }
     }
     Button(
